5114f47f4e3b3d2e6f965e719c816292f3699fd3
ivanpobeguts <Ivan.Pobeguc@gmail.com>
2018-06-17 19:54:35 +0300

    add loading all attemps from devman

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,20 @@
+import requests
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    base_url = 'https://devman.org/api/challenges/solution_attempts/'
+    number_of_pages = requests.get(base_url).json()['number_of_pages']
+    for page in range(1, number_of_pages + 1):
+        users_info = requests.get(base_url + '?page=' + str(page))
+        page_records_data = users_info.json()
+        for record in page_records_data['records']:
+            yield record
+
 
 def get_midnighters():
     pass
 
+
 if __name__ == '__main__':
-  pass
+    for record in load_attempts():
+        print(record)
